44982892e0f4aa82517b938acfd39713cfac3ff3
maribelpreite <maribelpreite@gmail.com>
2025-05-27 22:24:25 +0200

    feature dataset csv file for model training

--- util/features_extract.py
+++ util/features_extract.py
@@ -8,22 +8,29 @@ from feature_C import get_color_vector
 
 
 # Configuration
-image_dir = '../data/imgs_part_1/'
+image_dir = '../data/imgs/'
 mask_dir = '../data/lesion_masks/'
 metadata_path = '../dataset.csv'
+process_images = "../good_images_list.csv"
 
 
-# Load main dataframe
+# Load both CSVs
 metadata = pd.read_csv(metadata_path)
+good_pics = pd.read_csv(process_images).rename(columns={"Filename": "img_id"})
 
+
+# Merge to keep only good pictures (left join keeps order of good_pics)
+filtered = good_pics.merge(metadata, on='img_id', how='left')
 
 # Create 'cancer' column: True if diagnosis is SCC, BCC, or MEL
-metadata['cancer'] = metadata['diagnostic'].isin(['BCC', 'MEL', 'SCC'])
+filtered['cancer'] = metadata['diagnostic'].isin(['BCC', 'MEL', 'SCC'])
 
+# Choose the columns were gonna need
+filtered = filtered[['img_id', 'patient_id', 'cancer']]
 
-# Preload existing filenames for fast lookup
-available_images = set(os.listdir(image_dir))
-available_masks = set(os.listdir(mask_dir))
+# # Preload existing filenames for fast lookup
+# available_images = set(os.listdir(image_dir))
+# available_masks = set(os.listdir(mask_dir))
 
 
 # Initialize list for all feature rows
@@ -31,7 +38,7 @@ rows = []
 counter = 0
 
 # Iterate and process each sample
-for _, row in metadata.iterrows():
+for _, row in filtered.iterrows():
     counter += 1
     print(counter)
     img_id = row['img_id']
@@ -42,45 +49,46 @@ for _, row in metadata.iterrows():
     mask_filename = img_id.replace('.png', '_mask.png')
     mask_path = os.path.join(mask_dir, mask_filename)
 
-    if os.path.basename(img_path) not in available_images or os.path.basename(mask_path) not in available_masks:
+    try:
+        img = cv2.imread(img_path)
+        if img is None:
+            continue
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        if mask is None:
+            continue
+
+        # THIS MIGHT BE REMOVED LATER ON!!!  (we ignore lesions that are multiple and small because the asymmetry border and color)  
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 1:
+            continue
+
+        # Extract features
+        asymmetry = mean_asymmetry(mask)
+        border = B_compactness(mask)
+        color_vector = get_color_vector(img, mask)  # array of 6 elements
+
+        # Build row
+        result = {
+            'patient_id': patient_id,
+            'img_id': img_id,
+            'cancer': cancer,
+            'border': border,
+            'asymmetry': asymmetry
+        }
+
+        color_labels = ['White', 'Black', 'Red', 'Light-brown', 'Dark-brown', 'Blue-gray']
+        for i, label in enumerate(color_labels):
+            result[f'{label}'] = color_vector[i]
+
+        rows.append(result)
+
+    except:
         continue
-
-    img = cv2.imread(img_path)
-    if img is None:
-        continue
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-    if mask is None:
-        continue
-
-    # THIS MIGHT BE REMOVED LATER ON!!!  (we ignore lesions that are multiple and small because the asymmetry border and color)  
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 1:
-        continue
-
-    # Extract features
-    asymmetry = mean_asymmetry(mask)
-    border = B_compactness(mask)
-    color_vector = get_color_vector(img, mask)  # array of 6 elements
-
-    # Build row
-    result = {
-        'patient_id': patient_id,
-        'img_id': img_id,
-        'cancer': cancer,
-        'border': border,
-        'asymmetry': asymmetry
-    }
-
-    color_labels = ['White', 'Black', 'Red', 'Light-brown', 'Dark-brown', 'Blue-gray']
-    for i, label in enumerate(color_labels):
-        result[f'{label}'] = color_vector[i]
-
-    rows.append(result)
 
 # Final DataFrame
 features_df = pd.DataFrame(rows)
 
 # Store result
-features_df.to_csv('../data/feature_dataset.csv', index=False)
+features_df.to_csv('feature_dataset.csv', index=False)
