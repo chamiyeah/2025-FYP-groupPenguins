bee0dfbae47db22ada652270862bb71c9669b719
maribelpreite <maribelpreite@gmail.com>
2025-05-29 10:31:50 +0200

    trying out different feature extraction techniques

--- util/feature_C.py
+++ util/feature_C.py
@@ -7,6 +7,7 @@ from skimage.segmentation import slic
 from skimage.color import rgb2hsv
 from scipy.stats import circmean, circstd, entropy
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 
 # colors from KASMI2015CLASSIFICATION: https://ietresearch.onlinelibrary.wiley.com/doi/epdf/10.1049/iet-ipr.2015.0385
@@ -38,7 +39,7 @@ melanoma_hsv_colors = {'White': np.array([0.7183908 , 0.13364055, 0.85098039]),
                        'Dark-brown': np.array([0.05      , 0.96296296, 0.52941176]), 
                        'Blue-gray': np.array([0.69354839, 0.22302158, 0.54509804])}
 
-
+########################## STEP 1: Downsize for faster processing ########################33
 def downsizing(img, mask, downsizing_factor=0.4):
     #crucial step to improve running time
     img = cv2.resize(img, (int(img.shape[1] * downsizing_factor), int(img.shape[0] * downsizing_factor)), interpolation=cv2.INTER_AREA) #key step to improve running time
@@ -46,6 +47,7 @@ def downsizing(img, mask, downsizing_factor=0.4):
     return img, mask
 
 
+##################### STEP 2: Get chunks of pixels for faster processing based on color similarity ##################
 def slic_segmentation(image, mask, n_segments = 50, compactness = 0.1):
     #applies SLIC superpixel segmentation on the already segmented image. reduces dimensionality in the color data.
     #output is a 2D array the same size as the image, each pixel is labeled with a superpixel ID represented by an integer starting from 1.
@@ -101,6 +103,7 @@ def slic_segmentation(image, mask, n_segments = 50, compactness = 0.1):
 #     return color_counts / len(rgb_array)
 
 
+######################## STEP 3: Get the avg color in each superpixel ######################
 def get_hsv_means(hsv_image, slic_segments):
     flat_segments = slic_segments.flatten()
     flat_hsv = hsv_image.reshape(-1, 3)
@@ -123,10 +126,11 @@ def get_hsv_means(hsv_image, slic_segments):
 
         hsv_means.append(np.array([hue, sat, val]))
 
-    return hsv_means #each entry corresponds to one superpixel (labeled form 1 to n_segments) and contains its average HSV color.
+    return np.array(hsv_means) #each entry corresponds to one superpixel (labeled form 1 to n_segments) and contains its average HSV color.
 
 
-#getting superpixels only doesnt seem to work, so we need to get an avg color of the lesion as well as stad dev: the higher, the more irregular color is in the picture
+
+################# STEP 4: It calculates the avg HSV value across all superpixels and its std deviation, this will be key in the classification step ###################
 def avg_std_hsv(hsv_means):
     hsv_array = np.array(hsv_means)  # shape (n_superpixels, 3)
 
@@ -150,44 +154,93 @@ def avg_std_hsv(hsv_means):
     }
 
 
+
+################## STEP 5: calculate entropy across all superpixels ########################
+def color_entropy_from_superpixels(hsv_means, bins=10):
+    hues = np.array([h[0] for h in hsv_means])
+    hist, _ = np.histogram(hues, bins=bins, range=(0, 1), density=True)
+    return entropy(hist + 1e-6)
+
+
+#entropy acrosss pixels only (not sperpixels)
+def color_entropy_from_pixels(hsv_img, mask, bins=20):
+    h = hsv_img[:, :, 0][mask > 0]
+    hist, _ = np.histogram(h, bins=bins, range=(0, 1), density=True)
+    return entropy(hist + 1e-6)
+
+
+########### STEP 6: Get a number of dominant colors in the lesion using superpixels ################3
+
+#helper function: get a 2D matrix with rows corresponding to each superpixel: hue, sat, val, row, col (mean values + centroid position)
+def get_hsv_pos_features(hsv_means, slic_segments):
+    
+    features = []
+
+    for i in range(1, slic_segments.max() + 1): #considering slic_segments is a 2d array where 0's correspond with the mask, and then labels from 1 to 50 
+        mask = slic_segments == i # mask for the superpixel
+        coords = np.argwhere(mask)  # (row, col) positions
+        centroid = coords.mean(axis=0)  # (row_center, col_center)
+
+        hsv = hsv_means[i - 1] #get the hsv mean value for that super pixel
+        feature = np.concatenate([hsv, centroid])  # make it into a 5d feature: hue, sat, val, row, col ((mean),(position))
+        features.append(feature)
+
+    return np.array(features)
+
+
+#main: test different k values to get the best k (ie number of dominant colors in the image)
+def count_dominant_colors(features, k_range=range(2, 11)):
+    best_k = 2
+    best_score = -1
+
+    n_superpixels = features.shape[0]
+
+    if n_superpixels < 3:
+        return 1 if n_superpixels == 1 else 2 #impossible to calculate color clusters if no of superpixels is already low
+
+    for k in k_range:
+        if k >= n_superpixels: #maybe no of superpixels is already good as a color clusters measure
+            continue
+
+        kmeans = KMeans(n_clusters=k, random_state=0).fit(features)
+        labels = kmeans.labels_
+
+        if len(set(labels)) > 1:
+            try:
+                score = silhouette_score(features, labels) # the silhouette score measures how well-separated and cohesive the clusters are
+                if score > best_score:
+                    best_score = score
+                    best_k = k
+            except ValueError:
+                continue
+
+    return best_k
+
+
+############# STEP 7: find the number of melanoma colors according to Kasmi's approach ###############
 def match_melanoma_colors_hsv(hsv_means, kasmi_threshold=0.4):
     # assigns each superpixel to a melanoma color class if the color distance is below the threshold of 0.04 that Kasmi proposes in his paper.
-    hsv_array = np.array(hsv_means) 
     ref_array = np.array(list(melanoma_hsv_colors.values()))
     color_labels = list(melanoma_hsv_colors.keys())
 
-    color_counts = np.zeros(len(ref_array))
+    matched_colors = set()
 
-    for hsv in hsv_array:
-        # Circular-aware hue distance
-        dh = np.minimum(np.abs(hsv[0] - ref_array[:, 0]), 1 - np.abs(hsv[0] - ref_array[:, 0]))
+    for hsv in hsv_means:
+        dh = np.minimum(np.abs(hsv[0] - ref_array[:, 0]), 1 - np.abs(hsv[0] - ref_array[:, 0])) #hue distance (circular)
         ds = np.abs(hsv[1] - ref_array[:, 1])
         dv = np.abs(hsv[2] - ref_array[:, 2])
         distances = np.sqrt((dh) ** 2 + ds ** 2 + dv ** 2)
 
         min_idx = np.argmin(distances)
         if distances[min_idx] < kasmi_threshold:
-            color_counts[min_idx] += 1
-
-    # Normalize by total number of superpixels
-    proportions = color_counts / len(hsv_array)
-
-    return {label: prop for label, prop in zip(color_labels, proportions)}
+            matched_colors.add(color_labels[min_idx])
 
 
-def color_entropy(image, mask):
-    h = image[:, :, 0][mask > 0]  # only pixels inside the mask
-    hist, _ = np.histogram(h, bins=20, range=(0, 1), density=True)
-    return entropy(hist + 1e-6)  # small constant to avoid log(0)
+    return len(matched_colors) #number of melanoma colors present
 
 
-def count_dominant_colors(image, mask, n_clusters=5):
-    pixels = image[mask > 0].reshape(-1, 3)
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init='auto').fit(pixels)
-    return len(np.unique(kmeans.labels_))
-
-
-def get_color_vector(image, mask, downsizing_factor = 0.4, n_segments = 50, compactness = 0.1, kasmi_threshold=0.4):
+############ WRAPPER FUNCTION ###########
+def get_color_vector(image, mask, downsizing_factor = 0.4, n_segments = 50, compactness = 0.1):
 # main function that takes an RGB!! image and its mask as an input and provides a melanoma color proportion vector
 # IMPORTANT THAT THE BINARY MASK IS 2D    
 # downsize first!! improves running time significantly
@@ -197,8 +250,7 @@ def get_color_vector(image, mask, downsizing_factor = 0.4, n_segments = 50, comp
             'mean_H': np.nan, 'std_H': np.nan,
             'mean_S': np.nan, 'std_S': np.nan,
             'mean_V': np.nan, 'std_V': np.nan,
-            'White': np.nan, 'Black': np.nan, 'Red': np.nan,
-            'Light-brown': np.nan, 'Dark-brown': np.nan, 'Blue-gray': np.nan
+            'color_entropy': np.nan, 'dominant_colors': np.nan
         }
 
     image, mask = downsizing(image, mask, downsizing_factor)
@@ -211,11 +263,12 @@ def get_color_vector(image, mask, downsizing_factor = 0.4, n_segments = 50, comp
 
     #get the feature vector values separately
     hsv_stats = avg_std_hsv(hsv_means)
-    melanoma_props = match_melanoma_colors_hsv(hsv_means, kasmi_threshold)
+    # melanoma_props = match_melanoma_colors_hsv(hsv_means, kasmi_threshold)
 
-    result = {**hsv_stats, **melanoma_props}
-    result['color_entropy'] = color_entropy(hsv_img, mask)
-    result['dominant_colors'] = count_dominant_colors(hsv_img, mask)
+    result = hsv_stats
+    #result['color_entropy_sup'] = color_entropy_from_superpixels(hsv_means)
+    result['color_entropy'] = color_entropy_from_pixels(hsv_img, mask)
+    # result['dominant_colors'] = count_dominant_colors(get_hsv_pos_features(hsv_means, slic_segments))
+    result['melanoma_colors'] = match_melanoma_colors_hsv(hsv_means)
 
-    # Merge and return
     return result
